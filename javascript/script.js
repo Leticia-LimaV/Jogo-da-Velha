e4b57bf8ab5f01973757a9dbe6af2c638dccdb91
Letícia Lima <leticialimav@outlook.com.br>
2025-08-13 14:10:28 -0300

    Adição de estilo

--- javascript/script.js
+++ javascript/script.js
@@ -4,15 +4,21 @@ const celulas = document.getElementsByClassName('celula');
 let vezDoJogador = 'X';
 let jogoAtivo = true;
 
+//Combinações
+
 const combinacoesVencedoras = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
 ];
+
+//Evento de click
 
 for( let i = 0; i < celulas.length; i++) {
   celulas[i].addEventListener("click", function () {
     if(!jogoAtivo || celulas[i].textContent !== "") return;
 
     this.textContent = vezDoJogador;
+
+    this.classList.add(vezDoJogador.toLowerCase());
 
     if(verificarVitoria(vezDoJogador)) {
       setTimeout(() => alert(`${vezDoJogador} venceu!`), 50);
@@ -36,9 +42,12 @@ function verificarEmpate() {
   return Array.from(celulas).every(celula => celula.textContent !== "");
 }
 
+//Botão de reiniciar
+
 document.getElementById('reiniciar').addEventListener('click', function () {
   for (let i = 0; i < celulas.length; i++) {
     celulas[i].textContent = "";
+    celulas[i].classList.remove('x', 'o');
   }
   vezDoJogador = 'X';
   jogoAtivo = true;
