508684b689347bc76835153992b94a149f229269
Letícia Lima <leticialimav@outlook.com.br>
2025-08-18 12:46:42 -0300

    Estilização dos vencedores e perdedores

--- javascript/script.js
+++ javascript/script.js
@@ -1,54 +1,72 @@
-const tabuleiro = document.getElementById('tabuleiro');
-const celulas = document.getElementsByClassName('celula');
+const tabuleiro = document.getElementById("tabuleiro");
+const celulas = document.getElementsByClassName("celula");
 
-let vezDoJogador = 'X';
+let vezDoJogador = "X";
 let jogoAtivo = true;
 
 //Combinações
 
 const combinacoesVencedoras = [
-  [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
 ];
 
 //Evento de click
 
-for( let i = 0; i < celulas.length; i++) {
+for (let i = 0; i < celulas.length; i++) {
   celulas[i].addEventListener("click", function () {
-    if(!jogoAtivo || celulas[i].textContent !== "") return;
+    if (!jogoAtivo || celulas[i].textContent !== "") return;
 
     this.textContent = vezDoJogador;
 
     this.classList.add(vezDoJogador.toLowerCase());
 
-    if(verificarVitoria(vezDoJogador)) {
+    if (verificarVitoria(vezDoJogador)) {
       setTimeout(() => alert(`${vezDoJogador} venceu!`), 50);
       jogoAtivo = false;
       return;
     }
-    if(verificarEmpate()) {
-    setTimeout(() => alert("Empate!", 50));
-    jogoAtivo = false;
-    return;
-  }  
+    if (verificarEmpate()) {
+      setTimeout(() => alert("Empate!", 50));
+      jogoAtivo = false;
+      return;
+    }
     vezDoJogador = vezDoJogador === "X" ? "O" : "X";
-  })
+  });
 }
 
 function verificarVitoria(jogador) {
-  return combinacoesVencedoras.some(combinacao => combinacao.every(index =>celulas[index].textContent === jogador));
+  for (let combinacao of combinacoesVencedoras) {
+    if (combinacao.every((index) => celulas[index].textContent === jogador)) {
+      combinacao.forEach((index) => celulas[index].classList.add("vencedora"));
+    for (let i = 0; i < celulas.length; i++) {
+    if (!combinacao.includes(i)) {
+      celulas[i].classList.add("perdedora")
+    }
+  }  
+    return true;
+    }
+  }
+  return false;
 }
 
 function verificarEmpate() {
-  return Array.from(celulas).every(celula => celula.textContent !== "");
+  return Array.from(celulas).every((celula) => celula.textContent !== "");
 }
 
 //Botão de reiniciar
 
-document.getElementById('reiniciar').addEventListener('click', function () {
+document.getElementById("reiniciar").addEventListener("click", function () {
   for (let i = 0; i < celulas.length; i++) {
     celulas[i].textContent = "";
-    celulas[i].classList.remove('x', 'o');
+    celulas[i].classList.remove("x", "o", "vencedora", "perdedora");
   }
-  vezDoJogador = 'X';
+  vezDoJogador = "X";
   jogoAtivo = true;
-})
+});
